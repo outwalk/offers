f5f52cbd1a723e4a5814d2e1091e5f4bf8deb18c
Oleksandr Avoiants <shhavel@gmail.com>
2016-01-17 12:28:38 +0200

    Add more Airbrake configs: environment, logger

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,4 +2,8 @@ Airbrake.configure do |config|
   config.host = ENV['ERRBIT_HOST']
   config.project_id = true
   config.project_key = ENV['ERRBIT_API_KEY']
+  config.root_directory = Rails.root
+  config.logger = Rails.logger
+  config.environment = Rails.env
+  config.ignore_environments = [:test]
 end

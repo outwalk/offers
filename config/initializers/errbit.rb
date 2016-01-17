b3842b75e8eb277a89ba640f3661a5ef9c2eec7a
Oleksandr Avoiants <shhavel@gmail.com>
2016-01-17 12:44:32 +0200

    Add Airbrake dummy project key for test environment

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.host = ENV['ERRBIT_HOST']
   config.project_id = true
-  config.project_key = ENV['ERRBIT_API_KEY']
+  config.project_key = ENV['ERRBIT_API_KEY'] || 'test_project_key'
   config.root_directory = Rails.root
   config.logger = Rails.logger
   config.environment = Rails.env
